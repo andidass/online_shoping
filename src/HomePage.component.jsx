fa5169b4dbc44799b7604199026f99e476708355
andy <andiardhiandg@gmail.com>
2020-05-05 22:18:40 +0800

    update 1

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>HATS</div>
+            <div className='subtitle'>SHOP NOW</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
